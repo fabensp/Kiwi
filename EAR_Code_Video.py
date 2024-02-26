b45098f51cf6b621f8fabd79ac6f1e29767ed347
pfabe <fabensp@lafayette.edu>
2024-02-26 12:57:56 -0500

    added pi-adapted version of code

--- EAR_Code_Video.py
+++ EAR_Code_Video.py
@@ -5,11 +5,11 @@ import time
 from imutils import face_utils
 from scipy.spatial import distance as dist
 from playsound import playsound
+import threading
 
 
 #Global Configuration Variables
 FACIAL_LANDMARK_PREDICTOR = "shape_predictor_68_face_landmarks.dat"  # path to dlib's pre-trained facial landmark predictor
-MINIMUM_EAR = 0.05    # Minimum EAR for both the eyes to mark the eyes as open
 
 #Initializations
 faceDetector = dlib.get_frontal_face_detector()     # dlib's HOG based face detector
@@ -20,8 +20,15 @@ landmarkFinder = dlib.shape_predictor(FACIAL_LANDMARK_PREDICTOR)  # dlib's landm
 (rightEyeStart, rightEyeEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
 # timer variable used to keep track of elasped time
-time = 0
+cooldown = 0
 ear = 0
+cooldown2 = 0
+sidelook_ratio = 0
+th_cd = 0
+th_cd2 = 0
+
+MINIMUM_EAR = 0.15
+COOLDOWN = 3
 
 def eye_aspect_ratio(eye):
     p2_minus_p6 = dist.euclidean(eye[1], eye[5])
@@ -30,16 +37,18 @@ def eye_aspect_ratio(eye):
     ear = (p2_minus_p6 + p3_minus_p5) / (2.0 * p1_minus_p4)
     return ear
 
+def beep():
+    playsound("BEEP.mp3")
+
 cap = cv2.VideoCapture(0)
 
 while True:
     _, frame = cap.read()
-    gray_big = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    scale = 10
+    scale = 50
 
     #get the webcam size
-    height, width = gray_big.shape
+    height, width, channels = frame.shape
 
     #prepare the crop
     centerX,centerY=int(height/2),int(width/2)
@@ -48,9 +57,13 @@ while True:
     minX,maxX=centerX-radiusX,centerX+radiusX
     minY,maxY=centerY-radiusY,centerY+radiusY
 
-    gray = gray_big#[minY:maxY, minX:maxX]
+    frame_small = frame[minY:maxY, minX:maxY]
 
+    gray = cv2.cvtColor(frame_small, cv2.COLOR_BGR2GRAY)
     faces = faceDetector(gray)
+
+    if not faces:
+        ear = 0.05
 
     for face in faces:
         faceLandmarks = landmarkFinder(gray, face)
@@ -64,6 +77,8 @@ while True:
 
         ear = (leftEAR + rightEAR) / 2.0
 
+        sidelook_ratio = p1_minus_p4 = dist.euclidean(leftEye[0], leftEye[3])/dist.euclidean(rightEye[0], rightEye[3])
+
         leftEyeHull = cv2.convexHull(leftEye)
         rightEyeHull = cv2.convexHull(rightEye)
 
@@ -71,20 +86,37 @@ while True:
         cv2.drawContours(frame, [rightEyeHull], -1, (255, 0, 0), 2)
 
         cv2.putText(frame, "EAR: {}".format(round(ear, 3)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    
+    th1 = threading.Thread(target=beep)    
+    if ear < MINIMUM_EAR and cooldown == 0:
+        cooldown = time.time() + COOLDOWN
+    elif ear < MINIMUM_EAR and time.time() >= cooldown:
+        cooldown = 0
+        th_cd = time.time() + 2
+        th1.start()
+    elif time.time() >= cooldown:
+        cooldown = 0
 
-        if ear < MINIMUM_EAR:
-            cv2.putText(frame, "Drowsiness", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        # for playing note.wav file
-        if time>2:
-            # start timer
-            # if statement that activates time after x number of seconds
-            playsound('BEEP.mp3')
-            time = 0
-        cv2.imshow("Frame", frame)
-    if ear <0.25:
-        time=time+1
-    else:
-        time=0
+    th2 = threading.Thread(target=beep)    
+    if (sidelook_ratio > 1.2 or sidelook_ratio < 0.8) and cooldown2 == 0:
+        cooldown2 = time.time() + COOLDOWN
+    elif (sidelook_ratio > 1.2 or sidelook_ratio < 0.8) and time.time() >= cooldown2:
+        cooldown2 = 0
+        th_cd2 = time.time() + 2
+        th2.start()
+    elif time.time() >= cooldown2:
+        cooldown2 = 0
+
+    if ear < MINIMUM_EAR or (sidelook_ratio > 1.2 or sidelook_ratio < 0.8):
+        cv2.putText(frame, "Drowsiness", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    if cooldown > 0:
+        cv2.putText(frame, "{}".format(round(cooldown - time.time(), 2)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        
+    cv2.putText(frame, "Eye Width Ratio: {}".format(round(sidelook_ratio, 2)), (200, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    cv2.imshow("Frame", frame)
+
     if cv2.waitKey(1) == ord('q'):
         break
 cap.release()
